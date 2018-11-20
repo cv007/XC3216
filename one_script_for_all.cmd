33274c3aa981dcdde710f04c3eebc13b37ce9b88
cv007 <32439630+cv007@users.noreply.github.com>
2018-11-20 17:58:34 -0600

    Add files via upload

--- one_script_for_all.cmd
+++ one_script_for_all.cmd
@@ -9,7 +9,7 @@ printf "*cc1:+ -mafrlcsj\n\n*cc1plus:+ -mafrlcsj" > specs
 for f in /opt/microchip/xc32/v?.??; do cp specs $f/lib/gcc/specs; done
 for f in /Applications/microchip/xc32/v?.??; do cp specs $f/lib/gcc/specs; done
 for f in /opt/microchip/xc16/v?.??; do cp specs $f/lib/xc16-specs; done
-for f in /Applications/microchip/xc32/v?.??; do cp specs $f/lib/xc-16specs; done
+for f in /Applications/microchip/xc16/v?.??; do cp specs $f/lib/xc-16specs; done
 rm specs
 exit 0
 
@@ -17,7 +17,7 @@ exit 0
 #windows will show an error for the first #! line in this script, that's ok
 :windows
 @echo off
-net file 2>nul 1>&2 || echo need to run batch file as admin && goto :eof
+net file >nul 2>&1 || echo need to run batch file as admin && goto :eof
 (echo *cc1:+ -mafrlcsj && echo. && echo *cc1plus:+ -mafrlcsj) > specs
 for /D %%f in ("C:\Program Files (x86)\Microchip\xc32\v?.??") do copy specs "%%f\lib\gcc\specs"
 for /D %%f in ("C:\Program Files (x86)\Microchip\xc16\v?.??") do copy specs "%%f\lib\xc16-specs"
