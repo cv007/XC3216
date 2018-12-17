a290e8ef4d29cc5dcd9d374ab619009aa17f4aaf
cv007 <32439630+cv007@users.noreply.github.com>
2018-12-16 22:35:36 -0600

    Update one_script_for_all.cmd

--- one_script_for_all.cmd
+++ one_script_for_all.cmd
@@ -1,24 +1,25 @@
 #!/bin/sh
-goto :windows
 
 #OSX and Linux
-#will show an error for the goto command above, that's ok
 #should get 2- 'No such file or directory'
-#if get 'Permission denied', need to sudo
-printf "*cc1:+ -mafrlcsj\n\n*cc1plus:+ -mafrlcsj" > specs
-for f in /opt/microchip/xc32/v?.??; do cp specs $f/lib/gcc/specs; done
-for f in /Applications/microchip/xc32/v?.??; do cp specs $f/lib/gcc/specs; done
-for f in /opt/microchip/xc16/v?.??; do cp specs $f/lib/xc16-specs; done
-for f in /Applications/microchip/xc16/v?.??; do cp specs $f/lib/xc-16specs; done
-rm specs
+#if get 'Permission denied', need to run as sudo
+
+specs="*cc1:+ -mafrlcsj\n\n*cc1plus:+ -mafrlcsj"
+for f in /opt/microchip/xc32/v?.??; do echo -e $specs > $f/lib/gcc/specs; done
+for f in /Applications/microchip/xc32/v?.??; do echo -e $specs > $f/lib/gcc/specs; done
+for f in /opt/microchip/xc16/v?.??; echo -e $specs > $f/lib/xc16-specs; done
+for f in /Applications/microchip/xc16/v?.??; do echo -e $specs > $f/lib/xc-16specs; done
 exit 0
 
-#windows batch file, the goto will skip the *nix script above to below label
-#windows will show an error for the first #! line in this script, that's ok
-:windows
+rem =============================================
+rem   FOR WINDOWS, DELETE ALL THE ABOVE LINES
+rem =============================================
 @echo off
 net file >nul 2>&1 || echo need to run batch file as admin && goto :eof
 (echo *cc1:+ -mafrlcsj && echo. && echo *cc1plus:+ -mafrlcsj) > specs
-for /D %%f in ("C:\Program Files (x86)\Microchip\xc32\v?.??") do copy specs "%%f\lib\gcc\specs"
-for /D %%f in ("C:\Program Files (x86)\Microchip\xc16\v?.??") do copy specs "%%f\lib\xc16-specs"
+for /D %%f in ("%ProgramFiles(x86)%\Microchip\xc32\v?.??") do copy specs "%%f\lib\gcc\specs"
+for /D %%f in ("%ProgramFiles(x86)%\Microchip\xc16\v?.??") do copy specs "%%f\lib\xc16-specs"
+rem in case you are running 32bit, or xc switches to 64bit
+for /D %%f in ("%ProgramFiles%\Microchip\xc32\v?.??") do copy specs "%%f\lib\gcc\specs"
+for /D %%f in ("%ProgramFiles%\Microchip\xc16\v?.??") do copy specs "%%f\lib\xc16-specs"
 del specs
